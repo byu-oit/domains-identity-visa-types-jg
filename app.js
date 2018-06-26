f7ae8c040f6662109f98fec34ab4bff7cfd9aa7e
JosephGiles <Supreme.Captain.Joseph@gmail.com>
2018-06-26 13:25:54 -0600

    hope it works

--- app.js
+++ app.js
@@ -39,3 +39,19 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
+//============================================================================
+const SansServer        = require('sans-server');
+const SansServerSwagger = require('sans-server-swagger');
+
+// create a sans-server instance and export it
+const api = SansServer();
+module.exports = api;
+
+// add swagger middleware to the sans-server instance.
+api.use(SansServerSwagger({
+    controllers: './controllers',
+    development: true,
+    swagger: './swagger.json'
+}));
